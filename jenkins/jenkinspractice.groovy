d9d07645bf5b792c013ead8e02e7d7c78fe3877c
Anjanachari <anjanachari3@gmail.com>
2025-06-13 13:01:38 +0530

    modified docker jenkins pipe

--- jenkins/jenkinspractice.groovy
+++ jenkins/jenkinspractice.groovy
@@ -2,55 +2,39 @@ pipeline {
     agent any
     
     environment {
-        DOCKER_CREDENTIALS_ID = '5d339e10-6bf8-4908-a48d-e9d02db872c7'
-        DOCKER_IMAGE_NAME = 'anjanachari/second_image'
-        DOCKER_REGISTRY = 'https://hub.docker.com' // Docker Hub registry URL
+        DOCKER_CREDENTIALS_ID = '57f23e61-e22d-4ade-a077-d7e7e87997cf'
+        DOCKER_IMAGE_NAME = 'second_image'  // image name without username prefix
+        DOCKER_REGISTRY = ''  // Docker Hub doesn't need registry URL here
         GIT_REPO_URL = 'https://github.com/Anjanachari/jenkins2.git'
     }
     
     stages {
         stage('Checkout') {
             steps {
-                // Clone the GitHub repository
                 git url: "${env.GIT_REPO_URL}", branch: 'main'
             }
         }
-       /* 
-        stage('Clean and Package') {
+        
+        stage('Build') {
             steps {
-                script {
-                    // Run the Maven build (adjust if using another build tool)
-                    sh 'mvn clean package'
-                }
-            }
-        }*/
-        stage('Build'){
-            steps{
                 sh 'mvn clean package'
             }
         }
+        
         stage('Build Docker Image') {
             steps {
-                script {
-                    // Build the Docker image using shell command
-                    sh "docker build -t ${env.DOCKER_IMAGE_NAME} ."
-                }
+                sh "docker build -t ${env.DOCKER_IMAGE_NAME} ."
             }
         }
         
         stage('Push Docker Image') {
             steps {
-                script {
-                    // Log in to Docker Hub
-                    withCredentials([usernamePassword(credentialsId: "${env.DOCKER_CREDENTIALS_ID}", passwordVariable: 'DOCKER_PASSWORD', usernameVariable: 'DOCKER_USERNAME')]) {
-                        sh "echo ${DOCKER_PASSWORD} | docker login ${env.DOCKER_REGISTRY} -u ${DOCKER_USERNAME} --password-stdin"
-                        
-                        // Tag the Docker image
-                        sh "docker tag ${env.DOCKER_IMAGE_NAME} ${DOCKER_USERNAME}/${env.DOCKER_IMAGE_NAME}:latest"
-                        
-                        // Push the Docker image to Docker Hub
-                        sh "docker push ${DOCKER_USERNAME}/${env.DOCKER_IMAGE_NAME}:latest"
-                    }
+                withCredentials([usernamePassword(credentialsId: "${env.DOCKER_CREDENTIALS_ID}", passwordVariable: 'DOCKER_PASSWORD', usernameVariable: 'DOCKER_USERNAME')]) {
+                    sh '''
+                        echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+                        docker tag ${DOCKER_IMAGE_NAME} ${DOCKER_USERNAME}/${DOCKER_IMAGE_NAME}:latest
+                        docker push ${DOCKER_USERNAME}/${DOCKER_IMAGE_NAME}:latest
+                    '''
                 }
             }
         }
